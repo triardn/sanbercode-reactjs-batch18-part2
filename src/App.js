6bdd04b8b177874e4432771814b3f568b4029c79
Tri Ardini <tr.ardn@gmail.com>
2020-10-09 22:08:10 +0700

    convert tugas 9 into a component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Form from './Tugas-9/form';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Form Pembelian Buah</h1>
-        <div className="table">
-          <table>
-            <tr>
-              <td style={{width: '45%'}}><strong>Nama Pelanggan</strong></td>
-              <td><input type="text" style={{width: '200px'}} /></td>
-            </tr>
-            <tr>
-              <td style={{verticalAlign: 'bottom'}}><strong>Daftar Item</strong></td>
-              <td>
-                <input type="checkbox" id="fruits" name="fruits" value="watermelon" />
-                <label for="fruits">Semangka</label><br/>
-                <input type="checkbox" id="fruits" name="fruits" value="orange" />
-                <label for="fruits">Jeruk</label><br/>
-                <input type="checkbox" id="fruits" name="fruits" value="pineapple" />
-                <label for="fruits">Nanas</label><br/>
-                <input type="checkbox" id="fruits" name="fruits" value="Snakefruit" />
-                <label for="fruits">Salak</label><br/>
-                <input type="checkbox" id="fruits" name="fruits" value="grape" />
-                <label for="fruits">Anggur</label><br/>
-              </td>
-            </tr>
-          </table>
-          <button>Kirim</button>
-        </div>  
+    <div>
+      <Form />
     </div>
   );
 }
