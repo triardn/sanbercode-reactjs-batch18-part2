0a74e1efed390879a1832fa136d29d5e19369c81
Tri Ardini <tr.ardn@gmail.com>
2020-10-17 00:53:43 +0700

    tugas-15

--- src/Tugas-14/form.js
+++ src/Tugas-14/form.js
@@ -1,4 +1,4 @@
-import React, {useContext, useState, useEffect} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {FruitListContext} from './provider';
 import axios from 'axios';
 
--- src/Tugas-15/Navigation/Nav.js
+++ src/Tugas-15/Navigation/Nav.js
@@ -6,26 +6,14 @@ import './style.css';
 const Nav = () => {
     return (
         <div className="topnav">
-            <div>
+            <div className="nav-item">
                 <Link to="/">Home</Link>
-            </div>
-            <div>
                 <Link to="/tugas-9">Tugas 9</Link>
-            </div>
-            <div>
                 <Link to="/tugas-10">Tugas 10</Link>
-            </div>
-            <div>
                 <Link to="/tugas-11">Tugas 11</Link>
-            </div>
-            <div>
                 <Link to="/tugas-12">Tugas 12</Link>
-            </div>
-            <div>
                 <Link to="/tugas-13">Tugas 13</Link>
-            </div>
-            <div>
-            <Link to="/tugas-14">Tugas 14</Link>
+                <Link to="/tugas-14">Tugas 14</Link>
             </div>
         </div>
     );
